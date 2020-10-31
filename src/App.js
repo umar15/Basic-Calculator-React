e310f1d2fa6d955883763134f52506b0a47c8b9e
Umar Khalid <umar.qur105@gmail.com>
2020-10-31 02:42:46 -0700

    Complete Code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import React, { useState } from "react";
 import "./index.css";
-
+import "./components.css";
 import Button from "./components/Button";
 import TextField from "./components/TextField";
 
@@ -10,24 +10,86 @@ function App() {
   const [history, setHistory] = useState();
 
   const handleKeyPress = (e) => {
-
-  }
+    if (!isNaN(e.key)) {
+      if (main === 0) {
+        setMain(e.key);
+      } else {
+        setMain(`${main}${e.key}`);
+      }
+    }
+  };
 
   const keydownPress = (e) => {
+    if (e.keyCode === 8) {
+      if (main !== 0) {
+        setMain(main.slice(0, -1) !== "" ? main.slice(0, -1) : 0);
+      }
+    }
+  };
 
-  }
+  const numberHandler = (number) => {
+    if (!isNaN(number)) {
+      if (main === 0) {
+        setMain(number);
+      } else if (history) {
+        if (history.charAt(history.length - 1) === "=") {
+          setMain(number);
+        }
+      } else {
+        setMain(`${main}${number}`);
+      }
+    }
+  };
 
-  const numberHandler = (e) => {
-
-  }
-
-  const operationHandler = (e) => {
-
-  }
+  const operationHandler = (char) => {
+    if (char !== ".") {
+      if (isNaN(char)) {
+        if (main !== 0) {
+          setHistory(`${main}${char}`);
+          setMain(0);
+        } else if (main === 0 && history != 0 && history.slice(0, -1) !== "-") {
+          const v = history.slice(0, -1);
+          setHistory(`${v}${char}`);
+        }
+      }
+    } else {
+      setMain(`${main}${char}`);
+    }
+  };
 
   const equalHandler = (e) => {
+    if (history && main !== 0) {
+      if (
+        history.charAt(history.length - 1) !== "=" &&
+        history.charAt(history.length - 1) !== "%"
+      ) {
+        const v1 = history.slice(0, -1);
+        const opr = history.charAt(history.length - 1);
+        setHistory(`${history}${main}=`);
+        const v2 = main;
 
-  }
+        switch (opr) {
+          case "+":
+            setMain(v1 + v2);
+            break;
+          case "-":
+            setMain(v1 - v2);
+            break;
+          case "*":
+            setMain(v1 * v2);
+            break;
+          case "/":
+            setMain(v1 / v2);
+            break;
+        }
+      }
+    } else if (history.charAt(history.length - 1) === "%") {
+      console.log("here");
+      const v1 = history.slice(0, -1);
+      setHistory(`${history}=`);
+      setMain(v1 / 100);
+    }
+  };
 
   return (
     <div className="container">
@@ -47,7 +109,8 @@ function App() {
         <Button onClick={() => operationHandler("%")} bg="c9c9c9" title="%" />
         <Button
           onClick={() => {
-            setMain(0); setHistory("");
+            setMain(0);
+            setHistory("");
           }}
           bg="c9c9c9"
           title="CE"
@@ -92,4 +155,3 @@ function App() {
 }
 
 export default App;
-
